e588996027b74bc4560f8c3d6ddfd9321343264b
Daniel Acosta <d4ni3L_15@hotmail.com>
2023-03-23 14:28:20 -0500

    Customize RegisterUserController, add UserController and UserCollection

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -13,29 +13,33 @@ use Illuminate\Validation\Rules;
 
 class RegisteredUserController extends Controller
 {
-    /**
-     * Handle an incoming registration request.
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function store(Request $request): Response
-    {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
-        ]);
+  /**
+   * Handle an incoming registration request.
+   *
+   * @throws \Illuminate\Validation\ValidationException
+   */
+  public function store(Request $request): Response
+  {
+    $request->validate([
+      'name' => ['required', 'string', 'max:255'],
+      'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
+      'password' => ['required', 'confirmed', Rules\Password::defaults()],
+    ]);
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
 
-        event(new Registered($user));
+    //LOS CAMBIOS SE PIERDE AL HACER breeze:install
+    $user = User::create([
+      'name' => $request->name,
+      'email' => $request->email,
+      'image' => '/default-avatar.jpeg',
+      'bio' => $request->bio,
+      'password' => Hash::make($request->password),
+    ]);
 
-        Auth::login($user);
+    event(new Registered($user));
 
-        return response()->noContent();
-    }
+    Auth::login($user);
+
+    return response()->noContent();
+  }
 }
--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 
 return [
 
-  /*
+    /*
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
@@ -15,31 +15,20 @@ return [
     |
     */
 
-  'paths' => [
-    'api/*',
-    'login',
-    'logout',
-    'register',
-    'user/password',
-    'forgot-password',
-    'reset-password',
-    'user/profile-information',
-    'email/verification-notification',
-    'sanctum/csrf-cookie'
-  ],
+    'paths' => ['*'],
 
-  'allowed_methods' => ['*'],
+    'allowed_methods' => ['*'],
 
-  'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
 
-  'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [],
 
-  'allowed_headers' => ['*'],
+    'allowed_headers' => ['*'],
 
-  'exposed_headers' => [],
+    'exposed_headers' => [],
 
-  'max_age' => 0,
+    'max_age' => 0,
 
-  'supports_credentials' => true,
+    'supports_credentials' => true,
 
 ];
